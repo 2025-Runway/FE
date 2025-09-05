33a10ad54b4eb157e371e10fb06c472b52296c77
yyj0917 <yyj0917@yonsei.ac.kr>
2025-09-06 02:37:14 +0900

    chore : typo css file add

--- src/app/(pages)/weather/page.tsx
+++ src/app/(pages)/weather/page.tsx
@@ -1,3 +1,9 @@
 export default function WeatherPage() {
-  return <div>WeatherPage</div>;
+  return (
+    <div className="w-full h-full flex flex-col">
+      <header className="py-3 w-full h-auto flex-center text-title1 text-gray-bk">
+        날씨
+      </header>
+    </div>
+  );
 }
