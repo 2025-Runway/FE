58e98d8500c359f8f43619f2822a28acbb3620d5
westofsky <westofsky159@gmail.com>
2025-09-06 17:56:40 +0900

    feat : add popular course, ai recommended course component

--- src/app/(pages)/home/page.tsx
+++ src/app/(pages)/home/page.tsx
@@ -1,12 +1,17 @@
-import { HomeHeader, RecentCourse } from './_components';
+import { HomeHeader, RecentCourse, CourseSection } from './_components';
 
 export default function HomePage() {
   return (
-    <main className='flex h-full w-full flex-col'>
+    <main className='flex w-full flex-col'>
       <HomeHeader />
-      <div className='py-6'>
+      <div className='py-8'>
         <RecentCourse />
       </div>
+      <hr className='bg-gray-0 h-2 w-full' />
+      <div className='py-4'>
+        <CourseSection />
+      </div>
+      <div className='h-[64px]' />
     </main>
   );
 }
