51e2ad4b15e4899e1e107190a5644f229a88dd20
westofsky <westofsky159@gmail.com>
2025-09-06 13:09:18 +0900

    feat : add recent course component

--- src/app/(pages)/home/page.tsx
+++ src/app/(pages)/home/page.tsx
@@ -1,9 +1,12 @@
-import { HomeHeader } from './_components';
+import { HomeHeader, RecentCourse } from './_components';
 
 export default function HomePage() {
   return (
     <main className='flex h-full w-full flex-col'>
       <HomeHeader />
+      <div className='py-6'>
+        <RecentCourse />
+      </div>
     </main>
   );
 }
