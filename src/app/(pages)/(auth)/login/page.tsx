be40595ec1e30c7d1f8146a6dffe7e8ac51439d1
yyj0917 <yyj0917@yonsei.ac.kr>
2025-09-11 23:05:44 +0900

    fix(login) : login page code 위치 및 주석추

--- src/app/(pages)/(auth)/login/page.tsx
+++ src/app/(pages)/(auth)/login/page.tsx
@@ -6,17 +6,23 @@ import { LoginBtnSection } from "../_components/login-btn-section";
 export default function LoginPage() {
   return (
     <>
+        {/* login page background image */}
         <div className="fixed inset-0 mobile-area bg-[url('/img/login/login-background.png')] bg-cover bg-center" />
+        
+        {/* login logo */}
+        <div className="fixed top-27 mobile-area flex-center">
+          <LoginLogo />
+        </div>
+
         <div className="relative w-full h-full">
-          <div className="fixed top-27 mobile-area flex-center">
-            <LoginLogo />
-          </div>
           <div className="w-full h-full flex flex-col justify-end gap-9">
             <LoginIntroTextEffect />
             <LoginBtnSection/>
           </div>
-          <div className="fixed bottom-0 mobile-area h-[50vh] bg-[linear-gradient(0deg,#0B111A_60%,rgba(14,47,47,0)_100%)] opacity-60" />
         </div>
+
+        {/* login background gradient */}
+        <div className="fixed bottom-0 mobile-area h-[50vh] bg-[linear-gradient(0deg,#0B111A_60%,rgba(14,47,47,0)_100%)] opacity-60" />
     </>
   );
 }
